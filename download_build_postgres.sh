3b4a23e6b8fbdeb56c833dadc25c72747d03d93e
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-04 11:21:03 +0900

    Update download_build_postgres.sh

--- download_build_postgres.sh
+++ download_build_postgres.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+VERSION=$1
 mkdir ~/workdir
 cd ~/workdir
-curl -O https://ftp.postgresql.org/pub/source/v$1/postgresql-$1.tar.bz2
-tar xjf postgresql-$1.tar.bz2
-cd postgresql-$1
+curl -O https://ftp.postgresql.org/pub/source/v$VERSION/postgresql-$VERSION.tar.bz2
+tar xjf postgresql-$VERSION.tar.bz2
+cd postgresql-$VERSION
 ./configure --prefix ~/workdir/db
 make
